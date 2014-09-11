12e132ef0e34279a7a8ed6c39910514b7bef155c
Michael Griffin <mdgriffin064@gmail.com>
2014-09-11 20:52:48 +0100

    Fixed indentation

--- demos/js/main.js
+++ demos/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
-		$('#js-main-slider').pogoSlider({
-			autoplay: true,
-			autoplayTimeout: 5000,
-			displayProgess: true,
-			preserveTargetSize: true,
-			targetWidth: 1000,
-			targetHeight: 300,
-			responsive: true
-		}).data('plugin_pogoSlider');
+	$('#js-main-slider').pogoSlider({
+		autoplay: true,
+		autoplayTimeout: 5000,
+		displayProgess: true,
+		preserveTargetSize: true,
+		targetWidth: 1000,
+		targetHeight: 300,
+		responsive: true
+	}).data('plugin_pogoSlider');
 
 	var transitionDemoOpts = {
 		displayProgess: false,
